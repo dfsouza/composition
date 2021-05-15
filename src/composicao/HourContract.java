2b092ee34563ff124db4fcd456fde8ae2d51a64b
Daniel Souza <danielfsouza@gmail.com>
2021-05-15 08:22:21 -0300

    Projeto composição

---
+++ src/composicao/HourContract.java
@@ -0,0 +1,45 @@
+package composicao;
+
+import java.util.Date;
+
+public class HourContract {
+	private Date date;
+	private double valuePerHour;
+	private int hour;
+	
+	public HourContract(Date date, double valuePerHour, int hour) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hour = hour;
+	}
+
+	public double totalValue() {
+       return (this.getHour() * this.getValuePerHour()); 
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	
+	
+}
